3f2734b63fd922a3b8f3798f426ae6342d6dcc2e
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-10-20 23:45:59 +0100

    More cleanups

--- ClientTopic.java
+++ ClientTopic.java
@@ -3,9 +3,6 @@ import java.io.DataInputStream;
 // Specialized class which handles reading a Topic from an input stream, thus receiving it.
 //
 public class ClientTopic extends Topic {
-    private ClientTopic(int id, String title, String body) {
-        super(id, title, body);
-    }
     private ClientTopic() {
         super();
     }
