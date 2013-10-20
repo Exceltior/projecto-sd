f81a3a3d094eb1e66cd0be965459d81c950e702d
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-10-20 23:44:41 +0100

    CLEAN UP UNUSED SHIT.

--- Idea.java
+++ Idea.java
@@ -1,7 +1,6 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
 
 public class Idea implements Serializable {
     public int getId() {
@@ -10,27 +9,6 @@ public class Idea implements Serializable {
 
     protected int id, uid;
     protected String body, title;
-    enum IdeaRelationship {IDEA_IN_FAVOR, IDEA_AGAINST}
-
-    // FIXME: These may not always be filled. For instance, when we only want to send topic titles
-    // to the server (list all ideas in a topic)
-    protected int[] parentTopicIDs = null;
-    protected int[] parentIdeaIDs = null;
-    protected IdeaRelationship[] parentIdeaRelationships = null;
-
-    //Children ideas and their relationships
-    protected int[] childrenIdeaIDs = null;
-    protected IdeaRelationship[] childrenIdeaRelationships = null;
-
- 	////
-    // Class Constructor
-    ////
-    public Idea(int id, int uid, String body, String title) {
-        this.id = id;
-        this.uid = uid;
-        this.body = body;
-        this.title = title;
-    }
 
     ////
     // Class Constructor
@@ -60,8 +38,6 @@ public class Idea implements Serializable {
     //  Method responsible for receiving messages from the Server, reading information from the data Output Stream
     ////
     public boolean readFromDataStream(DataInputStream in) {
-        int id, uid;
-        String title, body;
 
         if ( (this.id = Common.recvInt(in)) == -1)
             return false;
@@ -85,52 +61,6 @@ public class Idea implements Serializable {
         this.body = row[2];
         this.uid = Integer.valueOf(row[3]);
     }
-
-    ////
-    // Given a SQL row with all the parent topics (from table TopicosIdeias), add them to this idea as its parent
-    //
-    public boolean addParentTopicsFromSQL(ArrayList<String[]> lines) {
-        parentTopicIDs = new int[lines.size()];
-        for (int i = 0; i < lines.size(); i++) {
-            String[] row = lines.get(i);
-            parentTopicIDs[i] = Integer.valueOf(row[0]);
-        }
-
-        return true;
-    }
-
-    private IdeaRelationship relationshipFromInt(int relationship) {
-        if ( relationship == 1 )
-            return IdeaRelationship.IDEA_IN_FAVOR;
-        else if ( relationship == -1 )
-            return IdeaRelationship.IDEA_AGAINST;
-        else // NEVER GONNA HAPPEN
-            return IdeaRelationship.IDEA_IN_FAVOR;
-    }
-
-
-    public boolean addParentIdeasFromSQL(ArrayList<String[]> lines) {
-        parentIdeaIDs = new int[lines.size()];
-        for (int i = 0; i < lines.size(); i++) {
-            String[] row = lines.get(i);
-            parentIdeaIDs[i] = Integer.valueOf(row[0]);
-            parentIdeaRelationships[i] = relationshipFromInt(Integer.valueOf(row[1]));
-        }
-
-        return true;
-    }
-
-    public boolean addChildrenIdeasFromSQL(ArrayList<String[]> lines) {
-        childrenIdeaIDs = new int[lines.size()];
-        for (int i = 0; i < lines.size(); i++) {
-            String[] row = lines.get(i);
-            childrenIdeaIDs[i] = Integer.valueOf(row[1]);
-            childrenIdeaRelationships[i] = relationshipFromInt(Integer.valueOf(row[2]));
-        }
-
-        return true;
-    }
-
 
     public String toString(){
         return "Idea{" +
