2640fd01b214f1cd960bd89f010576ea04c8c31a
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-10-25 11:38:21 +0100

    Adding RMI robustness

--- RMIConnection.java
+++ RMIConnection.java
@@ -7,9 +7,42 @@ import java.rmi.registry.Registry;
  * We have this class so that there is a common interface (RMIConnection) that all clients and threads can use to
  * access the RMI. We can add reconnect stuff and the likes right here.
  */
-public class RMIConnection {
+public class RMIConnection extends Thread {
     private Registry RMIregistry = null;
     private RMI_Interface RMIInterface = null;
+    private static final int RMILongReconnectSleepTime = 5000;
+    boolean isDown = false;
+    private final Object isDownLock = new Object();
+
+    public void waitUntilRMIIsUp() {
+        System.out.println("Waiting for RMI to be back up...");
+        synchronized (isDownLock) {
+            while ( isDown )
+                try { isDownLock.wait(); } catch (InterruptedException e) {}
+        }System.out.println("RMI up!!");
+
+    }
+
+    public void run() {
+        for (;;) {
+            synchronized (isDownLock) {
+                while ( !isDown )
+                    try { isDownLock.wait(); } catch (InterruptedException e) {}
+
+                try { Thread.sleep(RMILongReconnectSleepTime); } catch (InterruptedException e) {}
+
+                //It's down, try to fix it
+                for(;;) {
+                    System.out.println("Long RMI reconnect thread...");
+                    if ( connect() )
+                        break;
+                    try { Thread.sleep(RMILongReconnectSleepTime); } catch (InterruptedException e) {}
+                }
+
+                System.out.println("RMI is back up!");
+            }
+        }
+    }
 
     synchronized boolean establishConnectionToRegistry() {
         try {
@@ -28,5 +61,27 @@ public class RMIConnection {
 
     synchronized RMI_Interface getRMIInterface() {
         return this.RMIInterface;
+    }
+
+    synchronized boolean connect() {
+        System.out.println("Trying connection to RMI...");
+        boolean val = false;
+        int count=0;
+        do {
+            val = establishConnectionToRegistry(); count++;
+            System.out.println("Attempt "+count+"..."+(val ? "Success!" : "Failed!"));
+            // sleep? FIXME
+            try { Thread.sleep(count*1000); } catch (InterruptedException e) {}
+        } while ( !val && count < 3);
+
+
+        if ( !val ) {
+            synchronized (isDownLock) {
+                isDown = true;
+                isDownLock.notifyAll();
+            }
+        }
+
+        return val;
     }
 }
