307afb045f76bc47f97c738f9625ff977023e25a
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-12-02 20:11:47 +0000

    Making some sense out of BuySharesAction. Fuck me, it was ghastly.

--- webapp/IdeaBroker/src/main/resources/actions/controller/BuySharesAction.java
+++ webapp/IdeaBroker/src/main/resources/actions/controller/BuySharesAction.java
@@ -1,37 +1,9 @@
 package actions.controller;
 
-public class BuySharesAction extends ClientAction {
-    private int ideaId;
-    private int targetNumberShares;
-    private float targetPrice;
-
+public class BuySharesAction extends AJAXAction {
     public String execute() throws Exception {
         super.execute();
 
         return SUCCESS;
-    }
-
-    public int getIdeaId() {
-        return ideaId;
-    }
-
-    public void setIdeaId(int ideaId) {
-        this.ideaId = ideaId;
-    }
-
-    public int getTargetNumberShares() {
-        return targetNumberShares;
-    }
-
-    public void setTargetNumberShares(int targetNumberShares) {
-        this.targetNumberShares = targetNumberShares;
-    }
-
-    public float getTargetPrice() {
-        return targetPrice;
-    }
-
-    public void setTargetPrice(float targetPrice) {
-        this.targetPrice = targetPrice;
     }
 }
