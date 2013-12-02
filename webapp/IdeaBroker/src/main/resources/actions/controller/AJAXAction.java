2cbc97ea7ffdf93670ef6721ef3e73084ed94665
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-12-02 20:10:45 +0000

    AJAXAction docs

--- webapp/IdeaBroker/src/main/resources/actions/controller/AJAXAction.java
+++ webapp/IdeaBroker/src/main/resources/actions/controller/AJAXAction.java
@@ -6,6 +6,13 @@ package actions.controller;
  * and append all getters to the JSON output. In particular, the .JSP is expecting a success boolean value which
  * indicates the success or failure of the operation. Note that if we removed the getter (isSuccess()),
  * our .jsp would have problems.
+ * NOTE: ALL SUBCLASSES MUST IMPLEMENT #isSuccess() like this:
+ * <code>
+ *     public boolean isSuccess() {
+ *         return super.isSuccess();
+ *     }
+ * </code>
+ *
  * FIXME: This documentation has to be improved
  */
 public class AJAXAction extends ClientAction{
