a103e20578a3af33cbd8ab53802d1da40a419ecf
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-12-03 00:20:01 +0000

    More skeleton in BuySharesAction

--- webapp/IdeaBroker/src/main/resources/actions/controller/BuySharesAction.java
+++ webapp/IdeaBroker/src/main/resources/actions/controller/BuySharesAction.java
@@ -1,9 +1,71 @@
 package actions.controller;
 
 public class BuySharesAction extends AJAXAction {
+
+    /**
+     * IN
+     */
+    private int     iid;
+    private float   maxPricePerShare;
+    private int     buyNumShares;
+    private boolean addToQueueOnFailure;
+    private float   targetSellPrice;
+
+
+    /**
+     * OUT
+     */
+    private String result;
+    private int    numSharesBought;
+    private int    numSharesFinal;
+    private int    totalSpent;
+
     public String execute() throws Exception {
         super.execute();
 
+        ajaxSuccess();
+        result = "OK";
+        numSharesFinal = 10000;
+
         return SUCCESS;
+    }
+    public boolean isSuccess() {
+        return super.isSuccess();
+    }
+
+    public void setIid(int iid) {
+        this.iid = iid;
+    }
+
+    public void setMaxPricePerShare(float maxPricePerShare) {
+        this.maxPricePerShare = maxPricePerShare;
+    }
+
+    public void setBuyNumShares(int buyNumShares) {
+        this.buyNumShares = buyNumShares;
+    }
+
+    public void setAddToQueueOnFailure(boolean addToQueueOnFailure) {
+        this.addToQueueOnFailure = addToQueueOnFailure;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public int getNumSharesBought() {
+        return numSharesBought;
+    }
+
+    public int getTotalSpent() {
+        return totalSpent;
+    }
+
+    public int getNumSharesFinal() {
+        return numSharesFinal;
+    }
+
+    public void setTargetSellPrice(float targetSellPrice) {
+        this.targetSellPrice = targetSellPrice;
     }
 }
