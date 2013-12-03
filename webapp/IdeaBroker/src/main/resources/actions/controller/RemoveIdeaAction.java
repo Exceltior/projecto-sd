51fbad171dc7955a543ebd9189b214255a7bf30d
João Ricardo Lourenço <jorl17.8@gmail.com>
2013-12-03 00:33:02 +0000

    Porting Joca's RemoveIdea to AJAX

--- webapp/IdeaBroker/src/main/resources/actions/controller/RemoveIdeaAction.java
+++ webapp/IdeaBroker/src/main/resources/actions/controller/RemoveIdeaAction.java
@@ -1,21 +1,20 @@
 package actions.controller;
 
-public class RemoveIdeaAction extends ClientAction{
+public class RemoveIdeaAction extends AJAXAction{
     private int iid;
 
     public void setIid(int temp){
         this.iid = temp;
     }
-    public int getIid(){
-        return this.iid;
-    }
 
-    public String execute() throws Exception{
+    public String execute() throws Exception {
         super.execute();
 
-        if (client.doRemoveIdea(iid) > 0)
-            return SUCCESS;
-        else
-            return ERROR;
+        setAjaxStatus(client.doRemoveIdea(iid) > 0);
+
+        return SUCCESS;
+    }
+    public boolean isSuccess() {
+        return super.isSuccess();
     }
 }
