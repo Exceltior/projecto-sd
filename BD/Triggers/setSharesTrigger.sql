5b7fda34d345f8399b1e6c749835b06cb87dd8b4
jpleitao <jocaleitao93@gmail.com>
2013-12-14 18:02:14 +0000

    AND WE HAVE THE FINAL TRIGGER

--- BD/Triggers/setSharesTrigger.sql
+++ BD/Triggers/setSharesTrigger.sql
@@ -1,7 +1,7 @@
 DROP TRIGGER setShares;
 
-CREATE OR REPLACE TRIGGER setShares AFTER UPDATE OF numshares ON "Share" FOR EACH ROW WHEN ( new.numshares = 0)
+CREATE OR REPLACE TRIGGER setShares AFTER UPDATE OF numshares ON "Share"
 DECLARE
 BEGIN
-  DELETE FROM "Share" WHERE iid = :OLD.iid and userid = :OLD.userid;
+  DELETE FROM "Share" WHERE numshares = 0;
 END;
