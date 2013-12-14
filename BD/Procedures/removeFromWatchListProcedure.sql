19dcbc5170377a3f8530c0f87e977e97ee73e32f
jpleitao <jocaleitao93@gmail.com>
2013-12-14 23:42:45 +0000

    Changed the removeFromWatchList Procedure

--- BD/Procedures/removeFromWatchListProcedure.sql
+++ BD/Procedures/removeFromWatchListProcedure.sql
@@ -1,6 +1,6 @@
-DROP PROCEDURE removeFromWatchListProcedure;
+DROP PROCEDURE removeFromWatchList;
 
-CREATE OR REPLACE PROCEDURE removeFromWatchListProcedure (idea_id IN NUMBER, user_id IN NUMBER, temp OUT NUMBER) IS
+CREATE OR REPLACE PROCEDURE removeFromWatchList (idea_id IN NUMBER, user_id IN NUMBER, temp OUT NUMBER) IS
 BEGIN
   temp := -1;
   DELETE FROM IdeiaWatchList WHERE userid = user_id and iid = idea_id;
